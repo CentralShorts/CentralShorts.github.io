234f2496926183aad90a5ed3a41a1c5b7e2fcc67
CentralShorts <123006919+CentralShorts@users.noreply.github.com>
2023-05-04 19:31:57 +0200

    Add files via upload

---
+++ assets/js/Cursor.js
@@ -0,0 +1,43 @@
+const expandables = document.querySelectorAll(".cursor-expandable");
+
+const cursor = document.querySelector("[data-cursor]");
+
+const cursorDot = document.querySelector("[data-cursor-dot]");
+const cursorOutline = document.querySelector("[data-cursor-outline]");
+
+window.addEventListener("mousemove", function(e) {
+    const posX = e.clientX;
+    const posY = e.clientY;
+
+    cursorDot.style.left = `${posX}px`;
+    cursorDot.style.top = `${posY}px`;
+
+    cursor.style.display = "block";
+
+    cursorOutline.animate({
+        left: `${posX}px`,
+        top: `${posY}px`
+    }, { duration: 200, fill: "forwards" });
+});
+
+expandables.forEach(element => {
+    element.addEventListener("mouseover", function() {
+        cursorDot.animate({
+            width: "11px",
+            height: "11px"
+        }, { duration: 200, fill: "forwards" });
+    });
+});
+
+expandables.forEach(element => {
+    element.addEventListener("mouseleave", function() {;
+        cursorDot.animate({
+            width: "5px",
+            height: "5px"
+        }, { duration: 100, fill: "forwards" });
+    });
+});
+
+document.addEventListener("mouseout", function() {
+    cursor.style.display = "none";
+});
